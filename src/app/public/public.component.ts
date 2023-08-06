5703d2565736dec56836e88d9938236c4ed86fd4
Philip Chalacama <filyp007lfcm117@gmail.com>
2023-08-06 16:41:52 -0500

    Home con paleta de color

--- src/app/public/public.component.ts
+++ src/app/public/public.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-public',
   templateUrl: './public.component.html',
-  styleUrls: ['./public.component.css']
+  styleUrls: ['./public.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class PublicComponent {
-  
+
 }
